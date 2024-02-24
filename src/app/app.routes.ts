b0f975bc72acdf5b7f899d46ab1339d85bbf9dd0
Topaz712 <tzam712@gmail.com>
2024-02-24 15:38:50 -0600

    ADDED: auth gaurd to todo-list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -6,5 +7,13 @@ export const routes: Routes = [
     path: 'login',
     loadComponent: () =>
       import('./login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'todo-list',
+    loadComponent: () =>
+      import('./todo-list/todo-list.component').then(
+        (m) => m.TodoListComponent
+      ),
+    canActivate: [authGuard],
   },
 ];
